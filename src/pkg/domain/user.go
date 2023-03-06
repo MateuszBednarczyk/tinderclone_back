2c1e4af2a6bf4407adc08f2a3eb3211cdf06db10
Mateusz Bednarczyk <matib004@gmail.com>
2023-03-06 14:50:51 +0100

    user entity update

--- src/pkg/domain/user.go
+++ src/pkg/domain/user.go
@@ -4,8 +4,10 @@ import "github.com/google/uuid"
 
 type User struct {
 	Id       uuid.UUID `gorm:"primary_key;type:uuid;default:gen_random_uuid()" json:"Id"`
-	Username string    `gorm:"uniqueIndex;not null" json:"Username"`
+	Username string    `gorm:"uniqueIndex;not null;type:varchar(25)" json:"Username"`
 	Password string    `gorm:"not null" json:"Password"`
-	Name     string    `gorm:"not null" json:"Name"`
-	Surname  string    `gorm:"not null" json:"Surname"`
+	Name     string    `gorm:"not null;type:varchar(25)" json:"Name"`
+	Surname  string    `gorm:"not null;type:varchar(30)" json:"Surname"`
+	Country  string    `gorm:"not null;type:varchar(25)" json:"Country"`
+	City     string    `gorm:"not null;type:varchar(25)" json:"City"`
 }
