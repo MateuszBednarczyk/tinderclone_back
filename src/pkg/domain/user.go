15ba5cbcd3e7c285f45f04d818b7fd17606b378e
Mateusz Bednarczyk <matib004@gmail.com>
2023-03-06 09:24:21 +0100

    user entity update

--- src/pkg/domain/user.go
+++ src/pkg/domain/user.go
@@ -6,4 +6,6 @@ type User struct {
 	Id       uuid.UUID `gorm:"primary_key;type:uuid;default:gen_random_uuid()" json:"Id"`
 	Username string    `gorm:"uniqueIndex;not null" json:"Username"`
 	Password string    `gorm:"not null" json:"Password"`
+	Name     string    `gorm:"not null" json:"Name"`
+	Surname  string    `gorm:"not null" json:"Surname"`
 }
