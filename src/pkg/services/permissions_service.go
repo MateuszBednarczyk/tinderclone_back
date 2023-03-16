5fe830291f6955757664e58eb0afd103edbb98f1
MateuszBednarczyk <m.bednarczyk@zsek.olsztyn.pl>
2023-03-16 14:38:34 +0100

    admin privileges giving upgrade

--- src/pkg/services/permissions_service.go
+++ src/pkg/services/permissions_service.go
@@ -20,7 +20,16 @@ func NewPermitter(userStore stores.IUserStore) *permitter {
 }
 
 func (s *permitter) GiveUserAdminPermission(username string) *Result {
-	err := s.userStore.UpdateUserRole(username, domain.Role(2))
+	var err error
+	user, err := s.userStore.SelectUserByUsername(username)
+	if err != nil {
+		return CreateServiceResult("User with given username does not exist", 404, []interface{}{})
+	}
+	if user.Role == domain.Role(2) {
+		return CreateServiceResult("User is already admin", 409, []interface{}{})
+	}
+
+	err = s.userStore.UpdateUserRole(username, domain.Role(2))
 	if err != nil {
 		return CreateServiceResult(err.Error(), 500, []interface{}{})
 	}
