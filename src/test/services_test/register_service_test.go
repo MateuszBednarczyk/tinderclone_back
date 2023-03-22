95aaf021cbd4d39d5bc00527d80aa6b6a1ac7814
MateuszBednarczyk <m.bednarczyk@zsek.olsztyn.pl>
2023-03-22 14:18:46 +0100

    tests

--- src/test/services_test/register_service_test.go
+++ src/test/services_test/register_service_test.go
@@ -3,7 +3,6 @@ package test
 import (
 	"testing"
 
-	"github.com/go-playground/assert/v2"
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
@@ -15,40 +14,29 @@ import (
 )
 
 func TestHashPassword(t *testing.T) {
-	//given
 	plainPass := "ADMIN"
-
-	//when
 	result, err := services.HashPassword(plainPass)
 
-	//then
-	assert.NotEqual(t, result, nil)
-	assert.Equal(t, err, nil)
+	require.NotEqual(t, result, nil)
+	require.Equal(t, err, nil)
 }
 
 func TestIsPasswordValid(t *testing.T) {
-	//given
 	pass := ""
-
-	//when
 	result := services.IsPasswordValid(pass)
 
-	//then
-	assert.Equal(t, result, false)
+	require.Equal(t, result, false)
 }
 
 func TestIsUsernameValid(t *testing.T) {
-	//given
 	u1 := ""
 	u2 := "AD"
 
-	//when
 	r1 := services.IsUsernameValid(u1)
 	r2 := services.IsUsernameValid(u2)
 
-	//then
-	assert.Equal(t, r1, false)
-	assert.Equal(t, r2, true)
+	require.Equal(t, r1, false)
+	require.Equal(t, r2, true)
 }
 
 func TestRegisterUser(t *testing.T) {
